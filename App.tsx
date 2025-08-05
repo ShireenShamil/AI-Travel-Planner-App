9378a2a8278e23a298a744d9cc7754e79e9e6934
ShireenShamil <shireenshamil11@gmail.com>
2025-08-05 11:22:51 +0530

    Initial commit of Android App development

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className='text-red-500'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  redText: {
+    color: 'red',
   },
 });
---
+++ navigation/TabNavigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const TabNavigator = () => {
+  return (
+    <View>
+      <Text>Tab Navigator</Text>
+    </View>
+  );
+};
+
+export default TabNavigator;
+
+const styles = StyleSheet.create({
+  container: {
+    // Add your styles here
+  },
+});
