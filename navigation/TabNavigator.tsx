0db2c08427d918805ce7fe6d57a884f1f42e4a5a
ShireenShamil <shireenshamil11@gmail.com>
2025-08-06 10:46:00 +0530

    Implement navigation with TabNavigator and add Profile screen; remove unused Babel and Metro config files

--- navigation/TabNavigator.tsx
+++ navigation/TabNavigator.tsx
@@ -1,21 +1,52 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import HomeStack from './HomeStack.js';
+import HomeStack from './HomeStack';
 import {Ionicons} from '@expo/vector-icons';
+import GuideStack from './GuideStack';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='home' component={HomeStack} options={{
+    <Tab.Navigator screenOptions={{ 
+      tabBarActiveTintColor: '#FF5722',
+      tabBarInactiveTintColor: '#666',
+      tabBarStyle: {
+        backgroundColor: '#fff',
+        borderTopWidth: 0,
+      },
+      tabBarIconStyle: { marginBottom: -3,
+      },
+      tabBarLabelStyle: { fontSize: 12 },
+      headerShown: false,
+     }}>
+      <Tab.Screen name='Home' component={HomeStack} options={{
         tabBarIcon ({ color, size }) {
           return (
             <Ionicons name="home" size={size} color={color} />
           );
         }
       }} />
+
+      <Tab.Screen name='Guides' component={GuideStack} options={{
+        tabBarIcon ({ color, size }) {
+          return (
+            <Ionicons name="book" size={size} color={color} />
+          );
+        }
+      }} />
+
+      <Tab.Screen name='Profile' component={ProfileScreen} options={{
+        tabBarIcon ({ color, size }) {
+          return (
+            <Ionicons name="person" size={size} color={color} />
+          );
+        }
+      }} />
+
+
     </Tab.Navigator>
   );
 };
