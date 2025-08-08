33da67bcc3d0c7ac2dcad14a198f38f2d356a20f
Shireen Shamil <168336936+ShireenShamil@users.noreply.github.com>
2025-08-08 09:41:47 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,9 +6,11 @@ import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+    
     <NavigationContainer>
       <TabNavigator />
     </NavigationContainer>
+    
   );
 }
 
