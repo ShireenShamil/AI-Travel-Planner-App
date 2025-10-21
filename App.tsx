b180d7618d14fa5701c07aa8048303c23ffd53b0
Shireen Shamil <168336936+ShireenShamil@users.noreply.github.com>
2025-10-21 19:27:31 +0530

    Add comment in App component
    Added a comment in the App component.

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    
+    //hello
     <NavigationContainer>
       <TabNavigator />
     </NavigationContainer>
